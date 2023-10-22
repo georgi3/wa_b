163bdeff30bcd3d8b5466329995ec41fd9096058
George Marchenko <george.marchenko44@gmail.com>
2023-10-22 12:54:46 -0400

    changed static url

--- backend/settings.py
+++ backend/settings.py
@@ -124,8 +124,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_URL = '/static/django/'
-
+STATIC_URL = "/static/"
 MEDIA_URL = '/images/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
