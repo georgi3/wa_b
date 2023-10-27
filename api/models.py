3c84ff8076e731dbfa81f62756a288c60b3b2db5
George Marchenko <george.marchenko44@gmail.com>
2023-10-27 13:17:49 -0400

    added notes field for volunteers

--- api/models.py
+++ api/models.py
@@ -93,6 +93,7 @@ class Volunteer(models.Model):
     is_dishwasher = models.BooleanField("Dishwasher", default=False)
     is_photographer = models.BooleanField("Photographer", default=False)
     joined_date = models.DateTimeField("Joined At", auto_now_add=True, null=True)
+    notes = models.TextField("Notes", blank=True, null=True)
 
     def __str__(self):
         if self.user_id:
