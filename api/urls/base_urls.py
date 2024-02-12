d0c975e0387ac83a06097921ed68255ad31554c9
George Marchenko <george.marchenko44@gmail.com>
2024-02-12 01:02:44 -0500

    added path that handles magic link confirmation

--- api/urls/base_urls.py
+++ api/urls/base_urls.py
@@ -4,5 +4,6 @@ from api.views import base_views
 
 urlpatterns = [
     path("content/galleries", base_views.get_wa_galleries, name="galleries"),
-    path("contact-form-submission", base_views.submit_contact_form, name='contact-form-submission')
+    path("contact-form-submission", base_views.submit_contact_form, name='contact-form-submission'),
+    path('confirm/<str:token>/', base_views.confirm_participation, name='confirm_participation')
 ]
