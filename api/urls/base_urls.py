a331e04fd4df7947727ec4ef3835e9aa22017a5b
George Marchenko <george.marchenko44@gmail.com>
2024-02-23 12:05:54 -0500

    updated url path for participation confirmation due to updated logic

--- api/urls/base_urls.py
+++ api/urls/base_urls.py
@@ -5,5 +5,5 @@ from api.views import base_views
 urlpatterns = [
     path("content/galleries", base_views.get_wa_galleries, name="galleries"),
     path("contact-form-submission", base_views.submit_contact_form, name='contact-form-submission'),
-    path('confirm/<str:token>/', base_views.confirm_participation, name='confirm_participation')
+    path('confirm-assignment', base_views.confirm_participation, name='confirm_participation')
 ]
